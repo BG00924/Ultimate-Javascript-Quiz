d4e20916270ada2640b6ed2b435ae74342168cfc
BG00924 <BG00924@users.noreply.github.com>
2020-07-10 13:23:54 -0500

    finished product

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,11 +8,25 @@ var pageContentEl = document.querySelector("#content")
 var timerEl = document.getElementById('seconds');
 //var mainEl = document.getElementById("main");
 var startBtn = document.getElementById('start-btn');
-
+var container = document.getElementById('container')
 var questionContainer = document.getElementById('question-container');
 var questionEl = document.getElementById('question');
 var answerBtnEl = document.getElementById('answer-btn');
 var rightOrWrong = document.getElementById('answer');
+var currentScore = document.getElementById('currentScore')
+var quizDone = document.getElementById('done');
+var finalScore = document.getElementById('score');
+var AA = document.getElementById('initials');
+var initials = document.getElementById('newInitials');
+var initialBtn = document.getElementById('initial-btn');
+var viewScore = document.getElementById('viewScore');
+var showScore = document.getElementById('savedScore');
+var showingScore = document.getElementById('saved-score');
+var goBack = document.getElementById('go-back-btn');
+var clearBtn = document.getElementById('clear-btn');
+var timer = document.getElementById('countdown');
+
+var timeLeft = 75;
 
 
 
@@ -83,7 +97,20 @@ function beginGame() {
     intro.classList.add('hide')
     
     currentQuestion = 0
+
     questionContainer.classList.remove('hide')
+
+    // var timeInterval = setInterval(function() {
+    //     if (timeLeft > 0 && currentQuestion < 5) {
+    //         //timerEl.textContent = timeLeft;
+    //         timeLeft--;
+    //         timerEl.textContent = timeLeft;
+    //     }  else {
+    //         //timerEl.textContent = "Time's up!";
+    //         clearInterval(timeInterval);
+    //         //score();
+    //     }
+    // }, 1000);
     
     countdown();
     nextQuestion();
@@ -98,18 +125,24 @@ function nextQuestion() {
 }
 
 function showQuestion() {
-    questionEl.innerText = questions[currentQuestion].question;
-    questionEl.classList.add('qText')
-    questions[currentQuestion].answers.forEach((answer, i) => {
-        var id = i + 1
-        //console.log(id);
-        var button = document.createElement('button')
-        button.innerText = answer.text;
-        button.classList.add('btn', 'aText');
-        button.setAttribute("id", id);
-        button.addEventListener('click', answerChosen);
-        answerBtnEl.appendChild(button);
-    })
+    if(currentQuestion === questions.length) {
+        score();
+        //clearInterval(timeInterval);
+    }
+    else {
+        questionEl.innerText = questions[currentQuestion].question;
+        questionEl.classList.add('qText')
+        questions[currentQuestion].answers.forEach((answer, i) => {
+            var id = i + 1
+            //console.log(id);
+            var button = document.createElement('button')
+            button.innerText = answer.text;
+            button.classList.add('btn', 'aText');
+            button.setAttribute("id", id);
+            button.addEventListener('click', answerChosen);
+            answerBtnEl.appendChild(button);
+        })
+    }  
 }
 
 
@@ -128,12 +161,12 @@ function answerChosen() {
     }
 
     else {
-        //console.log('incorrect')
         rightOrWrong.innerHTML = ''
         var para = document.createElement('p')
         para.innerText = "Wrong!"
         para.classList.add('rightOrWrong')
         rightOrWrong.appendChild(para);
+        timeLeft = Math.max(0, timeLeft - 10);
     }
     currentQuestion++
     nextQuestion();
@@ -141,21 +174,62 @@ function answerChosen() {
 
 // Timer that counts down from 75
 function countdown() {
-    var timeLeft = 75;
     var timeInterval = setInterval(function() {
-        if (timeLeft > 1) {
-            timerEl.textContent = timeLeft;
+        if (timeLeft > 0 && currentQuestion < questions.length) {
             timeLeft--;
-        } else if (timeLeft === 1) {
             timerEl.textContent = timeLeft;
-            timeLeft--;
-        } else {
-            timerEl.textContent = "Time's up!";
+        }  else {
             clearInterval(timeInterval);
+            score();
         }
     }, 1000);
 };
 
-//startBtn.addEventListener("click", countdown);
+function score() {
+    container.classList.add('flex-c')
+    intro.classList.add('hide')
+    questionContainer.classList.add('hide')
+    timer.classList.add('hide')
+    currentScore.classList.remove('hide')
+    quizDone.innerText = "All done!"
+    finalScore.innerText = "Your final score is " + timeLeft + "."
+};
 
+function saveScore() {
+    localStorage.setItem('timeLeft', timeLeft);
+    localStorage.setItem('initials', initials.value);
+};
+
+function displayScore() {
+    intro.classList.add('hide')
+    questionContainer.classList.add('hide')
+    currentScore.classList.add('hide')
+    showScore.classList.remove('hide')
+    var savedScore = localStorage.getItem('timeLeft');
+    var savedInitials = localStorage.getItem('initials')
+    if (savedScore === null || savedInitials === null) {
+        showingScore.innerHTML = "No saved score"
+    }
+    else {
+        showingScore.innerHTML = savedInitials + "-" + savedScore
+    }
+    
+}
+
+function back() {
+    window.location.reload();
+}
+
+function deleteScore() {
+    localStorage.clear(); 
+    displayScore();
+}
+
+
+
+
+initialBtn.addEventListener("click", saveScore);
+viewScore.addEventListener("click", displayScore);
+goBack.addEventListener("click", back);
+clearBtn.addEventListener("click", deleteScore);
 startBtn.addEventListener("click", beginGame);
